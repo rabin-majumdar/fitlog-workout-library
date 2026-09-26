c8e07324df3fc8cfe6818248db82982aebccb3eb
Robin-Majumdar <robinmajumdarofficial@gmail.com>
2026-09-26 18:33:22 +0600

    Added favicon

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // https://img.magnific.com/free-photo/portrait-anime-character-doing-fitness-exercising_23-2151666664.jpg?w=740
   images: {
     remotePatterns: [
       {
